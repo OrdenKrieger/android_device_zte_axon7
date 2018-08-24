2c9673b9d3ee38cd8e654ff90ba633c4064ee74c
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2018-08-24 02:33:33 +0200

    axon7: Kang thermal engine configuration from marlin
    * This device thermal setup is shit!

--- device.mk
+++ device.mk
@@ -385,9 +385,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-8996-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-normal.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-8996-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-perf.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
 
 # USB
 PRODUCT_PACKAGES += \
