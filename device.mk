4d285446bb082d0a83bf15aac60b27ad6950ee9f
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    axon7: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- device.mk
+++ device.mk
@@ -317,7 +317,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
